a91764207ebdf51f1c49d5ef2af92118315c45cc
Ayesha Ejaz <160338390+ayeshajaz@users.noreply.github.com>
2025-01-18 07:16:51 -0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
         <div className='px-8 py-12'>
           <div className='flex flex-col md:flex-row lg:ml-28'>
             <div className='w-full md:w-[60%] h-auto md:h-[580px] border border-black bg-[#2A254B] text-white px-4 md:px-12 py-16 md:py-12 flex flex-col justify-between'>
-              <div className='font-montserrat'>
+              <div>
                 <h1 className='text-xl md:text-3xl md:text-left font-montserrat'>
                   The furniture brand for the <br /> future, with the timeless designs
                 </h1>
-                <div className='flex justify-center md:justify-start'>
+                <div className='flex justify-center md:justify-start font-questrial'>
                   <button className='w-[170px] h-[56px] bg-gray-500 text-white border border-gray-600 font-bold mt-12'>
                     View collection
                   </button>
@@ -20,7 +20,7 @@ const Hero = () => {
               </div>
 
               <div className='my-4 md:my-0'>
-                <p className='text-sm md:text-base md:text-left'>
+                <p className='text-sm md:text-base md:text-left font-questrial'>
                   A new era in eco-friendly furniture with Avion, the French luxury retail brand
                   <br /> with sleek fonts, full colors, and a beautiful way to display things digitally
                   <br /> using modern web technologies.
